22981ed0f2dab78a2472df67d9ab44cd1b225d2a
codeghoul <jayesh.patel.2197@gmail.com>
2020-04-14 11:12:01 +0530

    Integrate Test Containers in Tests.

---
+++ src/test/java/me/jysh/springcicddemo/testcontainer/PostgreSqlTestContainer.java
@@ -0,0 +1,37 @@
+package me.jysh.springcicddemo.testcontainer;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class PostgreSqlTestContainer extends PostgreSQLContainer<PostgreSqlTestContainer> {
+  private static final String IMAGE_VERSION = "postgres:11.1";
+  private static PostgreSqlTestContainer container;
+
+  private PostgreSqlTestContainer() {
+    super(IMAGE_VERSION);
+  }
+
+  /**
+   * Get Postgre SQL Test Container Instance.
+   *
+   * @return
+   */
+  public static PostgreSqlTestContainer getInstance() {
+    if (container == null) {
+      container = new PostgreSqlTestContainer();
+    }
+    return container;
+  }
+
+  @Override
+  public void start() {
+    super.start();
+    System.setProperty("DB_URL", container.getJdbcUrl());
+    System.setProperty("DB_USERNAME", container.getUsername());
+    System.setProperty("DB_PASSWORD", container.getPassword());
+  }
+
+  @Override
+  public void stop() {
+    // do nothing, JVM handles shut down
+  }
+}
